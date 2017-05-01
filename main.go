df2c2f7789a0b8f755a09d0c77c3d80e864cf576
daniiltserin <daniilborowkow@yandex.ru>
2017-05-01 11:56:40 +0600

    slice

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	arr[0] = 143
 
 	var arr2 = [3]int{1, 3, 5}
-
-	fmt.Printf("first is %d\n", arr[0])
-	fmt.Printf("second elem of second array is %d \n", arr2[1])
+	var slice []int
+	slice = arr2[:2]
+	fmt.Printf("%d\n", len(slice)) //1
 }
