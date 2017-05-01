7d1130ff6f157cc8ecdde2716b0b87a9674ff92f
daniiltserin <daniilborowkow@yandex.ru>
2017-05-01 13:12:01 +0600

    oop basics

--- main.go
+++ main.go
@@ -5,16 +5,22 @@ import (
 )
 
 func main() {
-	type person struct {
-		name string
-		age  int
-	}
+	r1 := Rect{88, 67}
+	fmt.Printf("Area of r1 is %f m2\n", area(r1))
+	fmt.Printf("Perimetr is %f m\n", r1.perim())
+}
 
-	type student struct {
-		person
-		speciality string
-	}
+/**
+* Recatngle
+ */
+type Rect struct {
+	width, height float64
+}
 
-	mark := student{person{"Mark", 18}, "Marketolog"}
-	fmt.Printf("%s\n", mark.speciality)
+func area(r Rect) float64 {
+	return r.width * r.height
+}
+
+func (r Rect) perim() float64 {
+	return r.width*2 + r.height*2
 }
