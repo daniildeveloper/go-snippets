12904854121bb4c0bc6f68497a060b8909dc042d
daniiltserin <daniilborowkow@yandex.ru>
2017-05-01 13:16:39 +0600

    base interfaces and reflection

--- main.go
+++ main.go
@@ -2,25 +2,12 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func main() {
-	r1 := Rect{88, 67}
-	fmt.Printf("Area of r1 is %f m2\n", area(r1))
-	fmt.Printf("Perimetr is %f m\n", r1.perim())
-}
+	i := 1
+	t := reflect.TypeOf(i)
 
-/**
-* Recatngle
- */
-type Rect struct {
-	width, height float64
-}
-
-func area(r Rect) float64 {
-	return r.width * r.height
-}
-
-func (r Rect) perim() float64 {
-	return r.width*2 + r.height*2
+	fmt.Println(t)
 }
