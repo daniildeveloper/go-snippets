26ea0f6ab8b1a2b42af2f628acd45c042984693d
daniiltserin <daniilborowkow@yandex.ru>
2017-05-01 12:51:51 +0600

    struct all

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ func main() {
 		name string
 		age  int
 	}
-	var P person
-	P.name = "Daniil"
-	P.age = 21
-	fmt.Printf("My name is %s\n", P.name)
-	Mark := person{age: 21, name: "Mark"}
-	fmt.Printf("My name is %s\n", Mark.name)
 
+	type student struct {
+		person
+		speciality string
+	}
+
+	mark := student{person{"Mark", 18}, "Marketolog"}
+	fmt.Printf("%s\n", mark.speciality)
 }
