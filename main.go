04a28ed3c4631e8ce569dfd714374333bcc02612
daniiltserin <daniilborowkow@yandex.ru>
2017-05-01 12:48:10 +0600

    Base struct

--- main.go
+++ main.go
@@ -5,14 +5,15 @@ import (
 )
 
 func main() {
-	x := 3
-	y := 4
-	a, b := sumAndProduct(x, y)
+	type person struct {
+		name string
+		age  int
+	}
+	var P person
+	P.name = "Daniil"
+	P.age = 21
+	fmt.Printf("My name is %s\n", P.name)
+	Mark := person{age: 21, name: "Mark"}
+	fmt.Printf("My name is %s\n", Mark.name)
 
-	fmt.Printf("%d + %d = %d\n", x, y, a)
-	fmt.Printf("%d * %d = %d\n", x, y, b)
-}
-
-func sumAndProduct(A, B int) (int, int) {
-	return A + B, A * B
 }
