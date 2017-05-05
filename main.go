72680390927c86ac3203fba727c1fed1c50adefd
daniiltserin <daniilborowkow@yandex.ru>
2017-05-05 14:05:04 +0600

    base server

--- main.go
+++ main.go
@@ -2,29 +2,45 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
 )
 
 func main() {
-	a := []int{7, 2, 8, -9, 4, 0}
-
-	c := make(chan int)
-
-	go sum(a[:len(a)/2], c)
-	go sum(a[len(a)/2:], c)
-
-	x, y := <-c, <-c
-
-	fmt.Println(x)
-	fmt.Println(y)
-	close(c)
-
+	http.HandleFunc("/", sayHelloName) //setting router rule
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
-func sum(a []int, c chan int) {
-	total := 0
+func sayHelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	//parse form to obtain data from
+	fmt.Println(r.Form) //print info on server side
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("shema", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
 
-	for _, v := range a {
-		total += v
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
 	}
-	c <- total
+	fmt.Fprintf(w, "Hello astaxie!") //write data to response
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method: ", r.Method) //get request method
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.html")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		//logic part of login
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
 }
